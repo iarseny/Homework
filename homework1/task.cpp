ed1ffc758651cbcc9e6b5104696676fd60ff6c2b
iarseny <122807698+iarseny@users.noreply.github.com>
2023-04-20 14:52:50 +0000

    homework

--- homework1/task.cpp
+++ homework1/task.cpp
@@ -1,1 +1,31 @@
+#include <iostream>
+#include <map>
 
+using namespace std;
+
+int main() {
+    int size;
+    cin >> size;
+    int *arr = new int[size];
+
+    for (int i = 0; i < size; i++) {
+        cin >> arr[i];
+    }
+
+    int target;
+    cin >> target;
+
+    map<int, int> m;
+
+    for (int i = 0; i < size; i++) {
+        if (m.find(target - arr[i]) != m.end()) {
+            cout << i << " " << m[target - arr[i]];
+            break;
+        }
+
+        m[arr[i]] = i;
+
+    }
+
+    return 0;
+}
